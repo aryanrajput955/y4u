c2fe7cb6710cc114d7e713225f7a449f21efa897
aryanrajput955 <aryan692000@gmail.com>
2026-03-11 15:14:36 +0530

    blogs admin

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,7 +1,7 @@
 import {NextResponse} from 'next/server'
 import {Resend} from 'resend'
 import dbConnect from '@/lib/mongodb'
-import Contact from '@/models/Contact'
+import Contact from '@/lib/models/Contact'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
